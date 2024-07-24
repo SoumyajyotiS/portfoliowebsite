8038bec4d3c011c23dbe6284ec7942eb1498e0ba
Soumyajyoti <166910220+Broken-Kaddu@users.noreply.github.com>
2024-07-25 00:04:16 +0530

    commit 2

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -31,8 +31,8 @@ const Contact = () => {
 
     emailjs
       .sendForm(
-        "service_94y20xo",
-        "template_v10u2oh",
+        "service_que1278",
+        "template_crlixec",
         formRef.current,
         "pX_2hasGmGcuvjXIW"
       )
